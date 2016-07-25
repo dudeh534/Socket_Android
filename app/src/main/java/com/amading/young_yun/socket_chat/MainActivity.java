f3d30fad9758a272ea97ff055c511a21d868abed
youngdo <youngdo9647@gmail.com>
2016-07-25 22:25:20 +0900

    todo list

--- app/src/main/java/com/amading/young_yun/socket_chat/MainActivity.java
+++ app/src/main/java/com/amading/young_yun/socket_chat/MainActivity.java
@@ -3,9 +3,17 @@ package com.amading.young_yun.socket_chat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/*1.manifests application android:name?
+/* 16/07/25
+* 1. manifests application android:name?
 * 2. application mean in project
 * 3. Fragment must use for project
+* 4. database study, mongo db structure study, express structure study
+* todo Chat room implement, random matching or users list
+* todo server service(mine), detail(event), exception web
+* todo fcm - push, auth - email, (analytics)
+* todo depth detail
+* todo Login api facebook
+* todo back-end MongoDB using - structure
  */
 
 public class MainActivity extends AppCompatActivity {
