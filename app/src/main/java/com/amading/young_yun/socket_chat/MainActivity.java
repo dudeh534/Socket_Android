4e94687f66e271efd0e5c008dfa061f1b3b2ce81
youngdo <youngdo9647@gmail.com>
2016-07-25 22:21:35 +0900

    todo list

--- app/src/main/java/com/amading/young_yun/socket_chat/MainActivity.java
+++ app/src/main/java/com/amading/young_yun/socket_chat/MainActivity.java
@@ -3,6 +3,11 @@ package com.amading.young_yun.socket_chat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*1.manifests application android:name?
+* 2. application mean in project
+* 3. Fragment must use for project
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
